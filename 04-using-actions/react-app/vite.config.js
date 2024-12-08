4c4bfde71ad9a573711baf762f24d62c0d899b81
Godson Nwaubani <nwaubanigodson2@gmail.com>
2024-12-09 00:20:53 +0100

    feat(04-using-actions): modify file for unit test workflow

--- 04-using-actions/react-app/vite.config.js
+++ 04-using-actions/react-app/vite.config.js
@@ -7,6 +7,6 @@ export default defineConfig({
   test: {
     globals: true,
     environment: 'jsdom',
-    setupFiles: resolve(__dirname, 'setupTests.js'), // Make sure path is correct
+    setupFiles: resolve(__dirname, './setupTests.js'),
   },
 });
