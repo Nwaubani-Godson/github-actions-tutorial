ac8ded278c1e24c1ad74d32e7260bac51afd7244
Godson Nwaubani <nwaubanigodson2@gmail.com>
2025-01-22 15:19:51 +0100

    feat(11-caching): modifying the configuration files

--- 11-caching/react-app/src/App.tsx
+++ 11-caching/react-app/src/App.tsx
@@ -16,7 +16,7 @@ function App() {
           <img src={reactLogo} className="logo react" alt="React logo" />
         </a>
       </div>
-      <h1>Vite + React</h1>
+      <h1>Hello my viewers, this is a Vite + React application</h1>
       <div className="card">
         <button onClick={() => setCount((count) => count + 1)}>
           count is {count}
@@ -25,6 +25,7 @@ function App() {
           Edit <code>src/App.tsx</code> and save to test HMR
         </p>
       </div>
+      <p>This is the application homepage</p>
       <p className="read-the-docs">
         Click on the Vite and React logos to learn more
       </p>
