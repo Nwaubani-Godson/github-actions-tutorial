6595f235bb8a1797944ce8a74ec3a5b875c3746f
Godson Nwaubani <nwaubanigodson2@gmail.com>
2025-03-05 16:01:03 +0100

    feat(12-artifacts): add workflow and react app

---
+++ 12-artifacts/react-app/src/App.test.tsx
@@ -0,0 +1,19 @@
+
+import { render, screen } from '@testing-library/react';
+import '@testing-library/jest-dom'
+import { describe, it, expect } from 'vitest';
+import App from './App';
+
+describe('App Component', () => {
+  it('renders the heading and paragraph correctly', () => {
+    render(<App />);
+
+    // Check if the <h1> element is rendered with the correct text
+    const heading = screen.getByRole('heading', { level: 1 });
+    expect(heading).toHaveTextContent('Hello my viewers, this is a Vite + React application');
+
+    // Check if the <p> element is rendered with the correct text
+    const paragraph = screen.getByText('This is the application homepage');
+    expect(paragraph).toBeInTheDocument();
+  });
+});
